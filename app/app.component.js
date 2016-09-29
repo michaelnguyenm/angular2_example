95bb097834b9d8bddee30c04935b33e59352efe7
Michael Nguyen <michaelmnguyen@utexas.edu>
2016-09-29 01:09:18 -0500

    Created necessary components/modules for app

---
+++ app/app.component.js
@@ -0,0 +1,15 @@
+//This is an IIFE, or Immediately-Invoked Function Expression
+//(function(){...}), parenthesis on the outside turns the function
+//into an expression. adding (); calls that function
+//Used to avoid "polluting the global namespace"
+//In otherwords, all variables inside the IIFE stay within that scope
+(function(app) {
+    app.AppComponent = //The visual component, "exports" to app namespace
+        ng.core.Component({
+            selector: 'my-app',
+            template: '<h1>Hello, World!</h1>'
+        })
+        .class({ //Ordinarily allows us to give properties/methods for this part of the UI
+            constructor: function(){} //Empty constructor
+        });
+})(window.app || (window.app{}));
